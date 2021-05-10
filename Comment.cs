cd4dcdd7e95d812395bde90f02abe3a02078efee
MikkelYtting <Mytting1996@gmail.com>
2021-05-10 12:55:31 +0200

    1

---
+++ Comment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CommentSection
+{
+    public class Comment
+    {
+        private int _id;
+        private string _commenttext;
+        
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+            }
+        }
+        public string Commenttext
+        {
+            get => _commenttext;
+            set
+            {
+                _commenttext = value;
+            }
+        }
+
+
+        public Comment(int Id, string Commenttext)
+        { 
+            this.Id = Id;
+            _commenttext = Commenttext;
+           
+        }
+        public Comment()
+        {
+
+        }
+    }
+}
